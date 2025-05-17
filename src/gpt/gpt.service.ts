31fab208dac5e55758fef5f8939872aed1354e57
Dieguidev <diegogaraycullas@gmail.com>
2025-05-17 09:28:57 -0500

    feat: add OpenAI integration for orthography check use case and update dependencies

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { ortographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
+import { envs } from 'src/config/envs';
+import OpenAI from 'openai';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: envs.openAiApiKey,
+  });
+
   async ortographyCheck(orthographyDto: OrthographyDto) {
     const { prompt } = orthographyDto;
-    return await ortographyCheckUseCase({ prompt });
+    return await ortographyCheckUseCase(this.openai, { prompt });
   }
 }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,13 +1,32 @@
+import OpenAI from 'openai';
 import { envs } from 'src/config/envs';
 
 interface Options {
   prompt: string;
 }
 
-export const ortographyCheckUseCase = async (options: Options) => {
+export const ortographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `Please check the following text for spelling and grammar errors, and provide a corrected version:\n\n${prompt}`,
+      },
+      {
+        role: 'user',
+        content: `Please check the following text for spelling and grammar errors, and provide a corrected version:\n\n${prompt}`,
+      },
+    ],
+    model: 'gpt-3.5-turbo',
+    // temperature: 0,
+  });
+
   return {
-    prompt,
-    apiKey: envs.openAiApiKey,
+    response: completion.choices[0],
   };
 };
